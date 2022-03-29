0d5953e51bfccd9d85e27c5bb24d590c4922053f
Naman Avasthi <naman.avasthi@gmail.com>
2022-03-29 17:20:20 +0530

    adding some storybook magic

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -6,13 +6,17 @@ import Button from "./Button";
 export default {
   title: "ReactComponentLibrary/Button",
   component: Button,
+  argTypes: {
+    onClick: { action: "Button Clicked" },
+    children: {
+      defaultValue: "Default Button Text",
+    },
+  },
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
 export const HelloWorld = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
 HelloWorld.args = {
   label: "Hello world!",
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,17 @@
 import React, { HTMLAttributes } from "react";
 
 export interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  /** used as button text */
   label: string;
 }
 
-const Button = (props: ButtonProps) => {
-  return <button className="uppercase bg-red-300 text-5xl">{props.label}</button>;
+/** cool little button */
+const Button = ({ label, ...rest }: ButtonProps) => {
+  return (
+    <button className="uppercase bg-red-300 text-5xl" {...rest}>
+      {label}
+    </button>
+  );
 };
 
 export default Button;
